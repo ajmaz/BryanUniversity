51005985212994a9122417deb86664d8af501478
ajmaz <adam.mazloum@bryanuniversity.edu>
2022-01-30 16:20:23 -0500

    quick fix

--- FSW-110/week5/submit_events2/index.js
+++ FSW-110/week5/submit_events2/index.js
@@ -3,17 +3,7 @@ var lname = document.getElementById("lname");
 var btn = document.getElementById("btn");
 var age = document.getElementById("age");
 var brk = "\r\n"
-var fav_col = "";
-if(document.theForm.color.checked){
-    fav_col = "Blue"
-}
-if(document.theForm.color1.checked){
-    fav_col += "Green"
-}
 
-if(document.theForm.color.checked){
-    fav_col += "Red"
-}
 
 btn.addEventListener("click", function(event) {
     alert("First name: " + fname.value + brk + 
