928eaebf630ba85d9acc2e215209780fc1fd1285
ajmaz <adam.mazloum@bryanuniversity.edu>
2022-02-06 15:28:00 -0500

    event work book

---
+++ FSW-110/week6/Event Workbook/click/app.js
@@ -0,0 +1,8 @@
+// make the box disapear when the user clicks it
+
+var click_me = document.getElementById('redbox');
+click_me.addEventListener("click", function(e){ 
+  click_me.style.display = 'none';
+  
+})
+
