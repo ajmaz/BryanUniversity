6e0505a5d14e34346b3265e85b1330a229b6c811
ajmaz <adam.mazloum@bryanuniversity.edu>
2022-04-12 10:17:07 -0400

    starwars api

--- FSW-113/week3/assignments/starwars/starwars.js
+++ FSW-113/week3/assignments/starwars/starwars.js
@@ -1,12 +1,23 @@
 // declare any necessary variables
+var queryType
+var itemID
+
 
 // define a function called 'fetchData()' that passes the values from 
 // the 'queryType' and 'itemID' elements in starwars.html to the function 
 // called 'getFromSWAPI()'
 
+function fetchData(){
+    queryType = document.querySelector("#queryType").value
+    itemID = document.querySelector("#itemID").value
+    getFromSWAPI()
+}
+
+
 function getFromSWAPI() {
     // assign values to any necessary variables
     fetch(`https://swapi.dev/api/${queryType}/${itemID}`)
+
     .then(function (response) {
         return response.json()
     })
@@ -17,6 +28,33 @@ function getFromSWAPI() {
         console.warn(err)
     })
 }
+
+var updateInfo = function(data){ 
+    if (queryType == "people" && itemID > 0 ){
+       document.querySelector("#dataLabel1").innerHTML = "Name: "
+       document.querySelector("#dataValue1").innerHTML = data.name 
+       document.querySelector("#dataLabel2").innerHTML = "Gender: "
+       document.querySelector("#dataValue2").innerHTML = data.gender 
+       
+    }
+    if (queryType == "planets" && itemID > 0 ){
+    document.querySelector("#dataLabel1").innerHTML = "Planet name: "
+    document.querySelector("#dataValue1").innerHTML = data.name 
+    document.querySelector("#dataLabel2").innerHTML = "Population: "
+    document.querySelector("#dataValue2").innerHTML = data.population 
+    }
+    if ( queryType === "starships" && itemID > 0 ){
+    document.querySelector("#dataLabel1").innerHTML = "Star ship: "
+    document.querySelector("#dataValue1").innerHTML = data.name 
+    document.querySelector("#dataLabel2").innerHTML = "Crew: "
+    document.querySelector("#dataValue2").innerHTML = data.crew 
+    }
+
+    else {
+        document.querySelector("#dataLabel1").innerHTML = "Please make a selection"
+    }
+}
+
 
 // create a new function called 'updateInfo()' that receives the data from 
 // the call to that function (see above). Use logic to write the appropriate
