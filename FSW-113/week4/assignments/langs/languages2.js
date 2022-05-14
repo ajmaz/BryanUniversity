d7d30dbb875f01c71f30c6222752ee66b06f78da
ajmaz <adam.mazloum@bryanuniversity.edu>
2022-05-14 14:22:10 -0400

    langs hw

--- FSW-113/week4/assignments/langs/languages2.js
+++ FSW-113/week4/assignments/langs/languages2.js
@@ -1,18 +1,32 @@
 const lang = 'JavaScript'
+// document.querySelector('#submit').addEventListener('click', function () {
+//     const inputList = [
+//         document.querySelector('#lang0').value,
+//         document.querySelector('#lang1').value,
+//         document.querySelector('#lang2').value,
+//         document.querySelector('#lang3').value,
+//         document.querySelector('#lang4').value,
+//         document.querySelector('#lang5').value,
+//         document.querySelector('#lang6').value,
+//         document.querySelector('#lang7').value
+//     ]
+//     chkLang(inputList)
+// })
 
-// Create an event listener for the submit button that adds all 'input' elements to 
-// a single array using the spread operator. Call the chkLang() function, passing in 
-// the array as an argument.
 
 
-function chkLang(langs) {
+function chkLang(lang, ...langs) {
     let result = false
-
     // use an array method to check whether the user included 'JavaScript' in their
     // list of languages
+    langs.forEach((item) => {
+        if (item == lang)
+            result = true
+
+    })
 
     let obj = document.querySelector('#TestResult')
-    if (result) 
+    if (result)
         obj.innerText = `Congratulations!\nYou know ${lang}.`
     else
         obj.innerText = `Sorry,\nyou don't know ${lang}.`
