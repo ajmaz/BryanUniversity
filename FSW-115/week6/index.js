ddda80ace7045ef7ee2b1a0ae90591465403c267
ajmaz <adam.mazloum@bryanuniversity.edu>
2022-05-29 15:50:19 -0400

    project 3 first submit

--- FSW-115/week6/index.js
+++ FSW-115/week6/index.js
@@ -1,16 +1,51 @@
-//promise chaining
-//url: https://swapi.dev/api/people/1
+const lukeContainer = document.querySelector(".lukeContainer")
+const movielist = document.querySelector("#movielist")
+const planetContainer = document.querySelector(".planetContainer")
 
 axios.get("https://swapi.dev/api/people/1")
     .then(res => {
-        const luke = res.data.name
-        console.log(luke)
+        const lukeInfo = res.data
+        console.log(lukeInfo)
+        let newHeading = document.createElement('h2')
+        newHeading.innerHTML = lukeInfo.name
+        lukeContainer.appendChild(newHeading)
         const homeworldUrl = res.data.homeworld
-        console.log(homeworldUrl)
         return axios.get(homeworldUrl)
     })
     .then(response => {
-        console.log(response.data)
-        return axios.get(response.data.films[0])})
+        const planet = response.data.name
+        planetContainer.innerHTML = planet
+        console.log(planet)
+
+        return axios.get(response.data.films[1])
+    })
     .then(res => console.log(res.data))
     .catch(err => console.log(err))
+
+
+
+
+
+async function getAllLukeMovies() {
+    const lukeData = await axios.get('https://swapi.dev/api/people/1')
+    const lukeFilms = lukeData.data.films
+    console.log(lukeData.data)
+    const pendingFilmsPromises = []
+    lukeFilms.forEach(film => {
+        pendingFilmsPromises.push(axios.get(film))
+    });
+
+
+    Promise.all(pendingFilmsPromises)
+        .then(res => {
+            res.forEach(film => {
+                let newFilm = document.createElement('li')
+                newFilm.innerHTML = film.data.title
+
+                movielist.appendChild(newFilm)
+            });
+        })
+        .catch(err => console.log(err))
+}
+
+getAllLukeMovies()
